77a46fb168901d45727eb3403e9563a3076cade7
jakebayliss <bayliss.jw@gmail.com>
2018-09-13 19:43:46 +1200

    Completing tasks with some sexy custom checkboxes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import '../styles/task.css';
 
 class Task extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            done: false
+        }
+    }
+
+    completeTask = () => {
+        if(this.state.done) {
+            this.setState({ done: false });
+            return;
+        }
+        this.setState({ done: true });
     }
 
     render(){
         return(
-            <li className="task">{this.props.text}</li>
+            <li className={this.state.done ? "task done" : "task"}>{this.props.text}<input className="task-checkbox" checked={this.state.done} type="checkbox" onChange={this.completeTask}/></li>
         );
     }
 }
